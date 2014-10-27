656de98ba837ccd7b07e2f574318ef986828f2bc
theway <theway_of@icloud.com>
2014-10-26 22:55:46 -0600

    Import data from old generator

---
+++ js/data.js
@@ -0,0 +1,619 @@
+var Data = {
+  "Nations": [
+    {
+      "name": "Australia",
+      "weight": 5,
+      "strikers": [
+        "Brewster Buffalo B-339E",
+        "Bristol Beaufighter Mk 21",
+        "CAC Boomerang Mk I",
+        "CAC Boomerang Mk II",
+        "Curtiss P-36 Mohawk",
+        "Curtiss P-40 Kittyhawk",
+        "de Havilland Mosquito",
+        "Fairey Firefly",
+        "P-51D Mustang",
+        "Supermarine Spitfire Mk VIII"
+      ],
+      "weapons": [
+        "Boys .55 AT Rifle",
+        "Lee-Enfield",
+        "M1917 Enfield",
+        "M1919 Browning machine gun",
+        "Owen submachine gun",
+        "Sten"
+      ]
+    },
+    {
+      "name": "Baltland",
+      "weight": 1,
+      "strikers": [
+        "Bristol Blenheim IV",
+        "Curtiss P-36 Hawk",
+        "Fairey Firefly",
+        "FFVS 22",
+        "Gloster Gladiator Mk I",
+        "Gloster Gladiator Mk II",
+        "Hawker Hurricane II",
+        "Hawker Tempest V",
+        "North American P-51D Mustang",
+        "Reggiane Re.2000 Serie I",
+        "Seversky EP-1 (P-35)",
+        "Stirling Mk. III",
+        "Supermarine Spitfire Mk VB"
+      ],
+      "weapons": [
+        "Karabiner 98k",
+        "Krag-Jørgensen",
+        "Lee-Enfield",
+        "M1 Carbine",
+        "M1917 Enfield",
+        "M1918 Browning Automatic Rifle",
+        "M/41",
+        "Sten",
+        "Thompson SMG",
+        "ZB vz. 26",
+        "Browning M1919",
+        "Colt M/29",
+        "Hotchkiss M1914",
+        "Hotchkiss M1914 machine gun",
+        "Ksp m/36",
+        "M/29",
+        "Madsen M/22"
+      ]
+    },
+    {
+      "name": "Belgium",
+      "weight": 1,
+      "strikers": [
+        "Brewster B-339 Buffalo",
+        "Dewoitine D.520",
+        "Fiat CR.42",
+        "Gloster Gladiator Mk I",
+        "Gloster Gladiator Mk II",
+        "Hawker Hurricane",
+        "Supermarine Spitfire"
+      ],
+      "weapons": [
+        "Gewehr 98",
+        "Hotchkiss M1914 machine gun",
+        "M1918 Browning Automatic Rifle",
+        "MG 08",
+        "Sten"
+      ]
+    },
+    {
+      "name": "Britannia",
+      "weight": 15,
+      "strikers": [
+        "Brewster B-339E Buffalo",
+        "Bristol Beaufighter",
+        "Curtiss P-36G Mohawk",
+        "Curtiss P-40 Kittyhawk",
+        "Curtiss P-40 Tomahawk",
+        "de Havilland Mosquito F Mk II",
+        "de Havilland Vampire Mk I",
+        "Fairey Firefly",
+        "Gloster Gladiator Mk I",
+        "Gloster Meteor",
+        "Grumman F6F Hellcat Mk I",
+        "Grumman F6F Hellcat Mk II",
+        "Grumman/General Motors F4F/FM Wildcat",
+        "Hawker Hurricane",
+        "Hawker Tempest",
+        "North American P-51D Mustang",
+        "Supermarine Spitfire",
+        "Vought F4U Corsair"
+      ],
+      "weapons": [
+        "Jungle Carbine",
+        "Lee-Enfield SMLE",
+        "M1 Carbine",
+        "M1917 Enfield",
+        "M1918 Browning Automatic Rifle",
+        "Pattern 14",
+        "Type 38 rifle",
+        "Lanchester",
+        "Owen submachine gun",
+        "Sten",
+        "Thompson M1",
+        "Thompson M1928",
+        "Thompson M1928A1",
+        "Thompson SMG",
+        "Besa",
+        "Bren LMG",
+        "Hotchkiss M1922 machine gun",
+        "Lewis LMG",
+        "M1919 Browning machine gun",
+        "M2 Browning",
+        "Vickers",
+        "Vickers K",
+        "Boys .55 AT Rifle"
+      ]
+    },
+    {
+      "name": "Bulgaria",
+      "weight": 0.3333,
+      "strikers": [
+        "Dewoitine D.520",
+        "Messerschmitt Bf109E",
+        "Messerschmitt Bf109G"
+      ],
+      "weapons": [
+        "Bren LMG",
+        "Carcano",
+        "MG 08",
+        "Mosin-Nagant",
+        "MP 40",
+        "Suomi KP/-31"
+      ]
+    },
+    {
+      "name": "Farawayland",
+      "weight": 5,
+      "strikers": [
+        "Boulton Paul Defiant",
+        "Curtiss P-40 Kittyhawk I",
+        "Curtiss P-40 Kittyhawk Ia",
+        "Curtiss P-40 Kittyhawk III",
+        "Curtiss P-40 Kittyhawk IV",
+        "de Havilland Mosquito",
+        "Fairey Firefly",
+        "Hawker Hurricane Mk X",
+        "Hawker Hurricane Mk XI",
+        "Hawker Hurricane Mk XII",
+        "Hawker Hurricane Mk XIIA",
+        "North American P-51 Mustang Mk I",
+        "North American P-51 Mustang Mk III",
+        "North American P-51 Mustang Mk IVA",
+        "Supermarine Spitfire"
+      ],
+      "weapons": [
+        "12mm M2 HMG",
+        "Boys .55 AT Rifle",
+        "Lee-Enfield",
+        "M1919 Browning machine gun",
+        "M1919A6",
+        "Sten",
+        "Thompson M1928",
+        "Thompson SMG"
+      ]
+    },
+    {
+      "name": "Fuso Empire",
+      "weight": 15,
+      "strikers": [
+        "Kawanishi N1K2-J",
+        "Kawasaki Ki-100",
+        "Kawasaki Ki-45",
+        "Kawasaki Ki-61",
+        "Mitsubishi A6M3a Zero",
+        "Nakajima Ki-43",
+        "Nakajima Ki-44",
+        "Nakajima Ki-79",
+        "Nakajima Ki-84 Hayate"
+      ],
+      "weapons": [
+        "Arisaka",
+        "Carcano",
+        "Mondragón rifle",
+        "Type 2",
+        "Type 44",
+        "Type 97",
+        "Nambu Type 100",
+        "Type 2",
+        "Type 1 HMG",
+        "Type 11 LMG",
+        "Type 3 HMG",
+        "Type 4 HMG",
+        "Type 92 HMG",
+        "Type 96 LMG",
+        "Type 97 LMG",
+        "Type 99 LMG"
+      ]
+    },
+    {
+      "name": "Gallia",
+      "weight": 5,
+      "strikers": [
+        "Arsenal VG39",
+        "Bloch MB.151",
+        "Bloch MB.152",
+        "Bloch MB.155",
+        "Bristol Blenheim",
+        "Curtiss Hawk 75A-4",
+        "Dewoitine D.520",
+        "Morane-Saulner M.S.406",
+        "Morane-Saulner M.S.410",
+        "Supermarine Spitfire Mk.V"
+      ],
+      "weapons": [
+        "Berthier Rifle",
+        "Karabiner 98k",
+        "Lebel Model 1886 Rifle",
+        "Lee-Enfield",
+        "M1917 Enfield",
+        "MAS-36",
+        "Bergman MP18",
+        "Erma EMP-35",
+        "MAS-38",
+        "Sten",
+        "Thompson M1928",
+        "Thompson SMG",
+        "Bren LMG Mk1",
+        "FM-24/29",
+        "Hotchkiss M1914",
+        "Hotchkiss M1922",
+        "M1919 Browning machine gun",
+        "Reibel Machine Gun",
+        "Boys .55 AT Rifle"
+      ]
+    },
+    {
+      "name": "Kingdom of Greece",
+      "weight": 0.3333,
+      "strikers": [
+        "Bloch MB.151",
+        "Bristol Blenheim Mk I",
+        "Gloster Gladiator Mk I",
+        "Supermarine Spitfire"
+      ],
+      "weapons": [
+        "Lee-Enfield",
+        "M1 Garand",
+        "M1918 Browning Automatic Rifle",
+        "Mannlicher Schoenauer",
+        "EPK machine gun",
+        "Thompson submachine gun",
+        "Hotchkiss M1914 machine gun",
+        "Hotchkiss M1922 machine gun",
+        "M1919 Browning machine gun",
+        "Schwarzlose MG M.07/12"
+      ]
+    },
+    {
+      "name": "Kingdom of Hispania",
+      "weight": 1,
+      "strikers": [
+        "Focke-Wulf Fw 190",
+        "Messerschmitt Bf 109"
+      ],
+      "weapons": [
+        "Erma EMP-35",
+        "Hotchkiss M1914 machine gun",
+        "Hotchkiss M1922 machine gun",
+        "Mosin-Nagant",
+        "vz. 24"
+      ]
+    },
+    {
+      "name": "Imperial Karlsland",
+      "weight": 15,
+      "strikers": [
+        "Focke-Wulf Fw 190-6",
+        "Dornier Do 335",
+        "Heinkel He 112",
+        "Heinkel He 162",
+        "Heinkel He 219",
+        "Junkers Ju 87 (StuKa)",
+        "Junkers Ju 88",
+        "Messerschmitt Bf 109F-4",
+        "Messerschmitt Bf 109G-2",
+        "Messerschmitt Bf 109G-6",
+        "Messerschmitt Bf 110",
+        "Messerschmitt Me 163",
+        "Messerschmitt Me 262"
+      ],
+      "weapons": [
+        "Carcano",
+        "Gewehr 41",
+        "Gewehr 43",
+        "Mauser Karabiner 98k",
+        "Mondragón rifle",
+        "vz. 24",
+        "vz. 33",
+        "Erma EMP-35",
+        "MP18/MP28",
+        "MP34",
+        "MP40",
+        "Suomi KP/-31",
+        "FM 24/29 light machine gun",
+        "MG08",
+        "MG131",
+        "MG15",
+        "MG151",
+        "MG30",
+        "MG34",
+        "MG42",
+        "ZB vz. 26",
+        "FG-42",
+        "StG 44",
+        "Panzerbüchse Boyes",
+        "PzB 39"
+      ]
+    },
+    {
+      "name": "Kiwi",
+      "weight": 2.5,
+      "strikers": [
+        "Brewster B-339E Buffalo",
+        "Curtiss P-36 Mohawk",
+        "Curtiss P-40 Kittyhawk",
+        "Hawker Hurricane",
+        "Hawker Tempest",
+        "Supermarine Spitfire",
+        "Vought F4U Corsair"
+      ],
+      "weapons": [
+        "Lee-Enfield",
+        "M1917 Enfield",
+        "Charlton Automatic Rifle",
+        "Owen submachine gun",
+        "Thompson M1928",
+        "Thompson SMG",
+        "Boys .55 AT Rifle"
+      ]
+    },
+    {
+      "name": "Orussian Empire",
+      "weight": 2.5,
+      "strikers": [
+        "Bereznyak-Isayev BI-1",
+        "Curtiss P-40 Kittyhawk",
+        "Curtiss P-40 Tomahawk",
+        "Ilyushin Il-10",
+        "Ilyushin Il-2",
+        "Lavochkin LaGG-3",
+        "Lavochkin La-5",
+        "Lavochkin La-7",
+        "MiG-1",
+        "MiG-3",
+        "Mig-60",
+        "Mikoyan-Gurevich I-250",
+        "Petlyakov Pe-3",
+        "Polikarpov I-185",
+        "Yakovlev Yak-1",
+        "Yakovlev Yak-3",
+        "Yakovlev Yak-7",
+        "Yakovlev Yak-9"
+      ],
+      "weapons": [
+        "AVS-36",
+        "M1918 Browning Automatic Rifle",
+        "Mondragón rifle",
+        "Mosin-Nagant M1891/30",
+        "Mosin-Nagant M1938",
+        "Mosin-Nagant M1944",
+        "Tokarev SVT-38",
+        "Tokarev SVT-40",
+        "Type 38 rifle",
+        "PPD-40",
+        "PPS submachine gun",
+        "PPSh-41",
+        "Thompson M1928",
+        "Thompson SMG",
+        "DP-28 LMG",
+        "DS-39",
+        "DshK 1936 HMG",
+        "Maxim PM1910 Medium Machine Gun",
+        "SG-43 Goryunov",
+        "Boys .55 AT Rifle",
+        "PTRD-41",
+        "PTRS-41"
+      ]
+    },
+    {
+      "name": "Ostmark",
+      "weight": 2.5,
+      "strikers": [
+        "Fiat CR.42",
+        "Messerschmitt Bf 109",
+        "Reggiane Re.2000"
+      ],
+      "weapons": [
+        "29M",
+        "30M",
+        "35M rifle",
+        "38M",
+        "43M rifle",
+        "45M",
+        "95M Mannlicher",
+        "M1918 Browning Automatic Rifle",
+        "Mondragón rifle",
+        "Danuvia 39M",
+        "Danuvia 43M",
+        "MP 34",
+        "PPSh-41",
+        "1934M Stange",
+        "1942M Grunov",
+        "42M",
+        "Madsel LMG",
+        "MG 08",
+        "Schwarzlose 7/31M heavy MG",
+        "Solothurn 31M light MG",
+        "Solothurn S-18/100"
+      ]
+    },
+    {
+      "name": "Phillipines",
+      "weight": 0.3333,
+      "strikers": [
+        "Boeing P-26 Peashooter"
+      ],
+      "weapons": [
+        "Boys .55 AT Rifle",
+        "M1917 Enfield",
+        "M1919 Browning machine gun",
+        "MG 08",
+        "Mondragón rifle",
+        "Thompson SMG",
+        "Type 38 rifle"
+      ]
+    },
+    {
+      "name": "Romagna",
+      "weight": 5,
+      "strikers": [
+        "Fiat CR.42",
+        "Fiat G.50",
+        "Fiat G.55 Centauro",
+        "Macchi MC.200",
+        "Macchi MC.202",
+        "Macchi MC.205",
+        "Messerschmitt Bf 109",
+        "Reggiane Re.2000",
+        "Reggiane Re.2001",
+        "Reggiane Re.2005"
+      ],
+      "weapons": [
+        "Carcano M1891",
+        "Carcano M1891TS",
+        "Carcano M1938 Carbine",
+        "Beretta Modello 1918",
+        "Moschetto Automatico Beretta 1938",
+        "OVP",
+        "Thompson SMG",
+        "Breda Modello 1937",
+        "Breda Modello 30",
+        "Fiat-Revelli Modello 1914",
+        "Fiat-Revelli Modello 1935",
+        "FM 24/29 light machine gun",
+        "M1919 Browning machine gun"
+      ]
+    },
+    {
+      "name": "South Africa",
+      "weight": 2.5,
+      "strikers": [
+        "Curtiss P-36 Mohawk",
+        "Curtiss P-40 Kittyhawk",
+        "Supermarine Spitfire"
+      ],
+      "weapons": [
+        "Lee-Enfield",
+        "M1919 Browning machine gun",
+        "Sten"
+      ]
+    },
+    {
+      "name": "Suomus",
+      "weight": 5,
+      "strikers": [
+        "Brewster B-239 Buffalo",
+        "Curtiss Hawk 75A-6",
+        "Hawker Hurricane",
+        "Messerschmitt Bf109G-2",
+        "Messerschmitt Bf109",
+        "Morane-Saulner M.S.406",
+        "Fiat G50",
+        "VL Myrsky"
+      ],
+      "weapons": [
+        "14mm pst kiv/37",
+        "Carcano",
+        "DP-28",
+        "KP/-31",
+        "Lahti L-39",
+        "LS/26",
+        "M1918 Browning Automatic Rifle",
+        "M1931 SMG",
+        "M24",
+        "M27",
+        "M28",
+        "M28-30",
+        "M39",
+        "M91",
+        "Maxim M/32-33",
+        "MG 08",
+        "MG42",
+        "Mosin-Nagant",
+        "MP 18",
+        "PPS SMG",
+        "Sten",
+        "SVT-40"
+      ]
+    },
+    {
+      "name": "United States of Liberion",
+      "weight": 15,
+      "strikers": [
+        "Brewster F2A Buffalo",
+        "Curtiss P-36 Hawk",
+        "Curtiss P-40",
+        "Curtis-Wright CW-21",
+        "Grumman/General Motors F4F/FM Wildcat",
+        "Grumman F6F Hellcat",
+        "Lockheed P-38 Lightning",
+        "Lockheed P-80 Shooting Star",
+        "North American P-51D Mustang",
+        "North American P-64",
+        "Northrop P-61 Black Widow",
+        "Republic P-43 Lancer",
+        "Republic P-47 Thunderbolt",
+        "Ryan FR Fireball",
+        "Seversky P-35",
+        "Supermarine Spitfire",
+        "Vought F4U Corsair",
+        "Vultee P-66 Vanguard"
+      ],
+      "weapons": [
+        "Lee-Enfield",
+        "M1 Carbine",
+        "M1 Garand",
+        "M1903 Springfield",
+        "M1917 Browning Automatic Rifle",
+        "M1917 Enfield rifle",
+        "M1918 Browning Automatic Rifle",
+        "M1941 Johnson rifle",
+        "Type 38 rifle",
+        "M3/A1",
+        "M50 Reising",
+        "Thompson M1",
+        "Thompson M1928",
+        "Thompson M1928A1",
+        "Thompson M1A1",
+        "Thompson SMG",
+        "United Defense M42",
+        "Browning M1917A1 HMG",
+        "Browning M1919 MMG",
+        "Browning M2 HMG",
+        "Hotchkiss M1914 machine gun",
+        "Lewis gun",
+        "M1941 Johnson",
+        "Boys .55 AT Rifle",
+        "M18 recoilless"
+      ]
+    },
+    {
+      "name": "Venezia",
+      "weight": 1,
+      "strikers": [
+        "Fiat CR.42",
+        "Fiat G.50",
+        "Fiat G.55 Centauro",
+        "Macchi MC.200",
+        "Macchi MC.202",
+        "Macchi MC.205",
+        "Messerschmitt Bf 109",
+        "Reggiane Re.2000",
+        "Reggiane Re.2001",
+        "Reggiane Re.2005"
+      ],
+      "weapons": [
+        "Carcano M1891",
+        "Carcano M1891TS",
+        "Carcano M1938 Carbine",
+        "Beretta Modello 1918",
+        "Moschetto Automatico Beretta 1938",
+        "OVP",
+        "Thompson SMG",
+        "Breda Modello 1937",
+        "Breda Modello 30",
+        "Fiat-Revelli Modello 1914",
+        "Fiat-Revelli Modello 1935",
+        "FM 24/29 light machine gun",
+        "M1919 Browning machine gun"
+      ]
+    }
+  ]
+};
